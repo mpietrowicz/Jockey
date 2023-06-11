b14288287e8dd7d0edebc4fb4a57c12f2851c1e4
Michał Pietrowicz <mp@mp.pl>
2023-06-11 16:45:52 +0200

    Simple menu

--- Jockey.Models/MainSettings.cs
+++ Jockey.Models/MainSettings.cs
@@ -8,13 +8,32 @@ public class MainSettings : ReactiveObject
    public ObservableCollectionExtended<MainMenuItem> MenuItems { get; set; }
     public MainSettings()
     {
+        InitializeMenu();
+    }
+
+    private void InitializeMenu()
+    {
         MenuItems = new()
         {
             new MainMenuItem()
             {
-                Header = "_File"
+                Header = "_File",
+                Items = new ObservableCollectionExtended<MainMenuItem>()
+                {
+                    new MainMenuItem()
+                    {
+                        Header = "_Open",
+                    },
+                     new MainMenuItem()
+                    {
+                        Header = "_Exit",
+                        Command = ReactiveCommand.Create(()=>
+                        {
+                            Environment.Exit(0);
+                        })
+                    }
+                }
             }
         };
-        
     }
 }
